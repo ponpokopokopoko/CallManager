4f149d9c061809b20906c0d437fb31277e784ae4
pokopoko <donnmeru22@gmail.com>
2025-08-14 22:08:09 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/CallManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallManagerApplication.class, args);
+	}
+
+}
